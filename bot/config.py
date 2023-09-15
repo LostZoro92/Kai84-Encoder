ed6a7964e20bf68dfd7489fed826e128f8254458
LostZoro92 <144257147+LostZoro92@users.noreply.github.com>
2023-09-15 07:55:09 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -16,18 +16,13 @@
 from decouple import config
 
 try:
-    APP_ID = config("APP_ID", cast=int)
-    API_HASH = config("API_HASH")
-    BOT_TOKEN = config("BOT_TOKEN")
+    APP_ID = config("APP_ID", "14874438")
+    API_HASH = config("API_HASH", "9d75f325568b6297204ef261040595f7")
+    BOT_TOKEN = config("BOT_TOKEN", "5387861278:AAF3CtbOu298iG522AY615UqtvxrDMT_XsU")
     DEV = 1322549723
-    OWNER = config("OWNER")
-    FFMPEG = config(
-        "FFMPEG",
-        default='ffmpeg -i "{}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{}"',
-    )
-    THUMB = config(
-        "THUMBNAIL", default="https://telegra.ph/file/75ee20ec8d8c8bba84f02.jpg"
-    )
+    OWNER = config("OWNER", "5178332815")
+    FFMPEG = config("FFMPEG","ffmpeg -i '''{}''' -preset faster -c:v libx265 -vf 'drawtext=fontfile=Aclonica.ttf:fontsize=27:fontcolor=white:bordercolor=black@0.50:x=w-tw-10:y=10:box=1:boxcolor=black@0.5:boxborderw=6:text=Omnimo3' -s 854x480 -crf 28 -map 0:v -c:a aac  -b:a 35k -map 0:a -c:s copy -map 0:s? '''{}''' -y")
+    THUMB = config("THUMBNAIL", "https://telegra.ph/file/75ee20ec8d8c8bba84f0)
 except Exception as e:
     LOGS.info("Environment vars Missing")
     LOGS.info("something went wrong")
